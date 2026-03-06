8e7c92b244f556458ef6298cba82275aab3c38a9
shantunu007 <chakmashantunu.web@gmail.com>
2026-03-06 21:51:36 +0600

    learned python basic variables and if-else

--- basic.py
+++ basic.py
@@ -1,10 +1,18 @@
 print("hello world");
 name = input("Enter your name: ")
-age = 30
+age = input("Enter your age: ")
 height = 5.6
 isStudent = True
+a = 5
+b = 6
+
+age = 30
+
+if(age >= 18): print("You can vote")
+else: print("You cannot vote")
 
 print("Hello", name)
 print(age)
 print(height)
 print(isStudent)
+print(a > b)
