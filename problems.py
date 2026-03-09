2788087c53231f0f8a608743632479d1167ba48c
shantunu007 <chakmashantunu.web@gmail.com>
2026-03-09 22:04:22 +0600

    Add Python loop practice: odd numbers and sum calculation

--- problems.py
+++ problems.py
@@ -17,3 +17,19 @@ for i in range(1, 21):
 user = int(input("Enter your number: "))
 for i in range(1, user+1): 
     print(i)
+
+# Problem 4
+# Print odd numbers from 1 to 30
+
+for i in range(1, 16):
+    if(i % 2 != 0):
+        print(i)  
+
+
+# Problem 5 
+# Find the sum of all numbers from 1 to 10       
+
+sum = 0
+for i in range(1, 11):
+      sum += i
+print(sum)
