13dc6fe9d1eaa512e7cfb25f0ab40f8acc2dbd74
shantunu007 <chakmashantunu.web@gmail.com>
2026-03-19 14:03:26 +0600

    add number guessing game with limited attempts and hints

--- numberGuessing.py
+++ numberGuessing.py
@@ -2,13 +2,19 @@ import random
 
 number = random.randint(1, 10)
 
-while True:
+attempts = 5
+    
+for i in range(attempts): 
     guess = int(input("Guess the number between(1 - 10): "))
     
     if guess == number: 
         print("You guessed it right")
         break
     elif guess > number: 
-        print("Too high")    
+        print("Too high")   
     else: 
-        print("Too low")    
+        print("Too low")
+    print("Chance left: ", attempts - 1 - i )  
+    
+else: 
+    print("Game Over. The number was: ", number) 
