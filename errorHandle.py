9aff814ec3cf0e215d1680830595a17ada68ab2c
shantunu007 <chakmashantunu.web@gmail.com>
2026-03-21 21:07:34 +0600

    Implement ValueError and ZeroDivisionError handling in user input

--- errorHandle.py
+++ errorHandle.py
@@ -5,5 +5,10 @@
 
 try: 
     num = int(input("Enter number: "))
-except: 
-    print("Invalid Input")    
+    print(10/num)
+    
+except ValueError: 
+    print("Please enter a number")  
+
+except ZeroDivisionError: 
+    print("Cannot divide by zero")      
