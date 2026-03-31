2a566d364e493ffe05bccacb96433a3c232d7b2a
shantunu007 <chakmashantunu.web@gmail.com>
2026-03-31 22:49:58 +0600

    Refactor: Use 'with' statement for file handling and read content

--- file.py
+++ file.py
@@ -1,12 +1,19 @@
-f = open("data.txt", "r")
-content = f.read()
-print(content)
-f.close()
+# f = open("data.txt", "r")
+# content = f.read()
+# print(content)
+# f.close()
 
-f = open("data.txt", "w")
-f.write("Hello boss. Welcome to python learning")
-f.close()
+# f = open("data.txt", "w")
+# f.write("Hello boss. Welcome to python learning")
+# f.close()
 
-f = open("data.txt", "a")
-f.write("\nNew line added")
-f.close()
+# f = open("data.txt", "a")
+# f.write("\nNew line added")
+# f.close()
+
+with open("data.txt", 'w') as file: 
+    file.write("Hello boss. Welcome to python learning")
+
+with open("data.txt", 'r') as file: 
+    content = file.read()
+    print(content)
