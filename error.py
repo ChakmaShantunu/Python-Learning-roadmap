15bc06835e68509d196f252d4b0644349e1be074
shantunu007 <chakmashantunu.web@gmail.com>
2026-04-03 21:04:59 +0600

    Handle specific ValueError in integer input

--- error.py
+++ error.py
@@ -3,3 +3,9 @@ try:
     print(num)
 except: 
     print("Invalid input")
+
+try: 
+    num2 = int(input("Enter number: "))
+    print(num2)
+except ValueError: 
+    print("Please enter a number: ")        
