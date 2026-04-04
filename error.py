bffe50f64c8f026431790bc9c8b4befa0b5d8731
shantunu007 <chakmashantunu.web@gmail.com>
2026-04-04 22:26:45 +0600

    Add exception handling for ZeroDivisionError

--- error.py
+++ error.py
@@ -9,3 +9,10 @@ try:
     print(num2)
 except ValueError: 
     print("Please enter a number: ")        
+
+try: 
+    a = int(input("Enter a number"))
+    b = int(input("Enter a number"))
+    print(a / b)
+except ZeroDivisionError: 
+    print("Cannot divided by zero")
