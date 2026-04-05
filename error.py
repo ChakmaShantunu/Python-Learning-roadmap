862e85d004b4470f1b4841e4dc8a20c19e38a0ce
shantunu007 <chakmashantunu.web@gmail.com>
2026-04-05 22:09:22 +0600

    Add try-except-finally block for exception handling

--- error.py
+++ error.py
@@ -11,8 +11,16 @@ except ValueError:
     print("Please enter a number: ")        
 
 try: 
-    a = int(input("Enter a number"))
-    b = int(input("Enter a number"))
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
     print(a / b)
 except ZeroDivisionError: 
     print("Cannot divided by zero")
+
+try: 
+    c = int("Enter a number: ")
+    print(c)
+except: 
+    print("Error")
+finally: 
+    print("Program finished")            
