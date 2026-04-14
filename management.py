6defc4da71239e732f8ab95db0aa3fe5e1d93a9c
shantunu007 <chakmashantunu.web@gmail.com>
2026-04-14 22:56:39 +0600

    create student list with name and mark input

---
+++ management.py
@@ -0,0 +1,22 @@
+students = []
+
+while True: 
+    print("\n1. add students")
+    print("2. view students")
+    print("3. average marks")
+    print("4. top students")
+    print("5. exist")
+    
+    choice = input("Enter choice: ")
+    
+    if choice == "1": 
+        name = input("Enter name: ")
+        mark = int(input("Enter mark: "))
+        
+        student = {
+            name: name,
+            mark: mark
+        }
+        students.append(student)
+        print("Students added")
+    
